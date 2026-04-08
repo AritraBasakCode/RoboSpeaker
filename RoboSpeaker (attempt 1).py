8681607f9c2e91376329248cad6e420702af542c
Aheli Roy <aheliroy2905@gmail.com>
2026-04-08 22:04:35 +0530

    upadated for windows

--- RoboSpeaker (attempt 1).py
+++ RoboSpeaker (attempt 1).py
@@ -1,4 +1,4 @@
-import os
+'''import os
 
 if __name__ == '__main__':
     print("Welcome to RoboSpeaker")
@@ -10,3 +10,21 @@ if __name__ == '__main__':
             break
         command = f"say {x}"
         os.system(command)
+        '''
+import pyttsx3
+
+engine = pyttsx3.init()
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker")
+
+    while True:
+        x = input("Enter what you want me to say: ")
+
+        if x == "q":
+            engine.say("bye bye for the time")
+            engine.runAndWait()
+            break
+
+        engine.say(x)
+        engine.runAndWait()
